c579ae55253233446d4bc4598de9b4d9f26c9706
Damon P. Cortesi <d.lifehacker@gmail.com>
2017-09-22 12:36:32 -0700

    Ugh, have to pull the templates into the code on the version of appengine I'm using. CSS still doesn't work.

--- app/app.go
+++ app/app.go
@@ -20,6 +20,7 @@ import (
 )
 
 func Run() {
+	http.Handle("/public/", http.FileServer(http.Dir("./public/")))
 	http.HandleFunc("/", indexPage)
 	http.HandleFunc("/current", currentText)
 	http.HandleFunc("/today", beersToday)
@@ -35,16 +36,235 @@ type PageVariables struct {
 	Tab   string
 }
 
-var weeklyBeerTemplate = template.Must(template.New("weeklybeer.html").Funcs(fns).ParseFiles(
-	"../app/weeklybeer.html",
-))
+var weeklyBeerTemplate = template.Must(template.New("weeklybeer.html").Funcs(fns).Parse(`
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+	<!-- Generated with d3-generator.com -->
+	<html>
+	  <head>
+	    <title>Chuck's Weekly</title>
+	    <meta http-equiv="X-UA-Compatible" content="IE=9">
+	    <script src="http://d3js.org/d3.v2.min.js"></script>
+	    <style>
+	      h4 {
+	        text-align: center;
+	        margin: 0;
+	      }
+	    </style>
+	  </head>
+	  <body>
+	    <table>
+	      <tr>
+	        <td>
+	          <div id="top_breweries"></div>
+	        </td>
+	        <td>
+	          <div id="top_styles"></div>
+	        </td>
+	      </tr>
+	      <tr>
+	        <td colspan="2"></td>
+	      </tr>
+	      <tr>
+	        <td>
+	          <div id="stale_beer"></div>
+	        </td>
+	        <td>
+	          <div id="fresh_kegs"></div>
+	        </td>
+	      </tr>
+	    </table>
+	    <script>
+	function renderChart(div, name, data) {
 
-var weeklyTemplate = template.Must(template.New("weekly.html").Funcs(fns).ParseFiles(
-	"../templates/weekly.html",
-	"../templates/bootstrap_base_head.html",
-	"../templates/navbar.html",
-	"../templates/bootstrap_base_foot.html",
-))
+	var valueLabelWidth = 40; // space reserved for value labels (right)
+	var barHeight = 20; // height of one bar
+	var barLabelWidth = 250; // space reserved for bar labels
+	var barLabelPadding = 5; // padding between bar and bar labels (left)
+	var gridLabelHeight = 5; // space reserved for gridline labels
+	var gridChartOffset = 0; // space between start of grid and first bar
+	var maxBarWidth = 420; // width of the bar with the max value
+
+	// accessor functions
+	var barLabel = function(d) { return d['Name']; };
+	var barValue = function(d) { return parseFloat(d['Value']); };
+
+	// scales
+	var yScale = d3.scale.ordinal().domain(d3.range(0, data.length)).rangeBands([0, data.length * barHeight]);
+	var y = function(d, i) { return yScale(i); };
+	var yText = function(d, i) { return y(d, i) + yScale.rangeBand() / 2; };
+	var x = d3.scale.linear().domain([0, d3.max(data, barValue)]).range([0, maxBarWidth]);
+	// svg container element
+	d3.select('#'+div).append("h4")
+	  .text(name)
+	var chart = d3.select('#'+div).append("svg")
+	  .attr('width', maxBarWidth + barLabelWidth + valueLabelWidth)
+	  .attr('height', gridLabelHeight + gridChartOffset + data.length * barHeight);
+
+	// bar labels
+	var labelsContainer = chart.append('g')
+	  .attr('transform', 'translate(' + (barLabelWidth - barLabelPadding) + ',' + (gridLabelHeight + gridChartOffset) + ')');
+	labelsContainer.selectAll('text').data(data).enter().append('text')
+	  .attr('y', yText)
+	  .attr('stroke', 'none')
+	  .attr('fill', 'black')
+	  .attr("dy", ".35em") // vertical-align: middle
+	  .attr('text-anchor', 'end')
+	  .text(barLabel);
+	// bars
+	var barsContainer = chart.append('g')
+	  .attr('transform', 'translate(' + barLabelWidth + ',' + (gridLabelHeight + gridChartOffset) + ')');
+	barsContainer.selectAll("rect").data(data).enter().append("rect")
+	  .attr('y', y)
+	  .attr('height', yScale.rangeBand())
+	  .attr('width', function(d) { return x(barValue(d)); })
+	  .attr('stroke', 'white')
+	  .attr('fill', 'steelblue');
+	// bar value labels
+	barsContainer.selectAll("text").data(data).enter().append("text")
+	  .attr("x", function(d) { return x(barValue(d)); })
+	  .attr("y", yText)
+	  .attr("dx", 3) // padding-left
+	  .attr("dy", ".35em") // vertical-align: middle
+	  .attr("text-anchor", "start") // text-align: right
+	  .attr("fill", "black")
+	  .attr("stroke", "none")
+	  .text(function(d) { return d3.round(barValue(d), 2); });
+	// start line
+	barsContainer.append("line")
+	  .attr("y1", -gridChartOffset)
+	  .attr("y2", yScale.rangeExtent()[1] + gridChartOffset)
+	  .style("stroke", "#000");
+
+	}
+	    </script>
+
+	    <script>
+
+	      var data = JSON.parse(unescape({{.TopBreweriesJson}}));
+	      renderChart("top_breweries", "Top Breweries", data);
+
+	      var data = JSON.parse(unescape({{.TopStylesJson}}));
+	      renderChart("top_styles", "Top Styles", data);
+
+	      var data = JSON.parse(unescape({{.MostStaleJson}}));
+	      renderChart("stale_beer", "Stale Beer", data);
+
+	      var data = JSON.parse(unescape({{.FastConsumedJson}}));
+	      renderChart("fresh_kegs", "Quickest Consumption", data);
+
+	    </script>
+	  </body>
+	</html>
+`))
+
+var weeklyTemplate = template.Must(template.New("weekly.html").Funcs(fns).Parse(`
+	{{template "bootstrap_base_head" .}}
+	<body>
+	  <div class="container">
+	    {{ template "navbar" .}}
+	    <div class="row">
+	      <h3 class="text-center">Stats for the past week at Chuck's!</h3>
+	    </div>
+	    <div class="row">
+	      <div class="col-md-4 col-md-offset-1">
+	        <h4 class="text-center">Top Breweries (by # on tap)</h4>
+	        {{ range .TopBreweries.Data }}
+	        {{.Name}}
+	        <div class="progress" style="margin-bottom: 4px;">
+	          <div class="progress-bar progress-bar-info" style="width: {{.Percentage}}%">{{ .Value }}</div>
+	        </div>
+	        {{ end }}
+	      </div>
+
+	      <div class="col-md-4 col-md-offset-2">
+	        <h4 class="text-center">Top Styles (by # on tap)</h4>
+	        {{ range .TopStyles.Data }}
+	        {{.Name}}
+	        <div class="progress" style="margin-bottom: 4px;">
+	          <div class="progress-bar progress-bar-info" style="width: {{.Percentage}}%">{{ .Value }}</div>
+	        </div>
+	        {{ end }}
+	      </div>
+
+	    </div>
+	  </div>
+
+	{{template "bootstrap_base_foot" .}}
+
+	{{define "bootstrap_base_head"}}
+	<!DOCTYPE html>
+	<html lang="en">
+	  <head>
+	    <meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <meta name="description" content="">
+	    <meta name="author" content="">
+	    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+
+	    <title>🍻 Chuck's Stats</title>
+
+	    <!-- Bootstrap core CSS -->
+	    <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+
+	    <!-- Custom styles for this template -->
+	    <link href="/public/css/slate.css" rel="stylesheet">
+	    <link href="/public/css/sticky-footer.css" rel="stylesheet">
+	    <link href="/public/css/fancy-badge.css" rel="stylesheet">
+
+	    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	    <!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+	  </head>
+	{{end}}
+
+	{{define "navbar"}}
+	      <div class="navbar navbar-default">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <a class="navbar-brand" href="/">Chuck's Stats</a>
+	        </div>
+	        <div class="navbar-collapse collapse navbar-responsive-collapse">
+	          <ul class="nav navbar-nav navbar-right">
+	            <li{{if eq .Page.Tab "today"}} class="active"{{end}}><a href="/today">Today</a></li>
+	            <li{{if eq .Page.Tab "thisweek"}} class="active"{{end}}><a href="/thisweek">This Week</a></li>
+	<!--             <li class="dropdown">
+	              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+	              <ul class="dropdown-menu">
+	                <li><a href="#">Chuck's 85th</a></li>
+	                <li><a href="#">Chuck's CD</a></li>
+	                <li class="divider"></li>
+	                <li><a href="#">Separated link</a></li>
+	              </ul>
+	            </li> -->
+	          </ul>
+	        </div><!-- /.nav-collapse -->
+	      </div>
+	{{ end }}
+
+	{{define "bootstrap_base_foot"}}
+
+	    <div id="footer">
+	      <div class="container">
+	        <p class="text-muted">Built for a love of beer, by <a href="https://twitter.com/dacort">@dacort</a>.</p>
+	      </div>
+	    </div>
+
+	    <!-- Bootstrap core JavaScript
+	    ================================================== -->
+	    <!-- Placed at the end of the document so the pages load faster -->
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+	  </body>
+	</html>
+	{{end}}
+`))
 
 type WeeklyBeerVariables struct {
 	TopBreweriesJson string
@@ -63,12 +283,129 @@ type WeeklyBeer2Variables struct {
 	Page PageVariables
 }
 
-var indexTemplate = template.Must(template.New("index.html").Funcs(fns).ParseFiles(
-	"../templates/index.html",
-	"../templates/bootstrap_base_head.html",
-	"../templates/navbar.html",
-	"../templates/bootstrap_base_foot.html",
-))
+var indexTemplate = template.Must(template.New("index.html").Funcs(fns).Parse(`
+	{{template "bootstrap_base_head" .}}
+
+	  <body>
+	    <div class="container">
+	      {{ template "navbar" .}}
+
+	      <!-- High-level stats -->
+	      <div class="row">
+	        <div class="col-md-3 col-md-offset-2">
+	          <div class="panel panel-default">
+	            <!-- Default panel contents -->
+	            <div class="panel-heading">
+	              <h3 class="panel-title">Fresh Taps!</h3>
+	            </div>
+	            <div class="panel-body">
+	              <p>Beers that have been tapped today.</p>
+	            </div>
+
+	            <!-- List group -->
+	            <ul class="list-group">
+	              {{ range .NewBeersToday }}
+	              <li class="list-group-item">{{ .FullName }}</li>
+	              {{ else }}
+	              <li class="list-group-item">Nothing new. :(</li>
+	              {{ end }}
+	            </ul>
+	          </div>
+	        </div>
+	        <div class="col-md-3 col-md-offset-2">
+	          <div class="panel panel-default">
+	            <!-- Default panel contents -->
+	            <div class="panel-heading">
+	              <h3 class="panel-title">Top Breweries This Week!</h3>
+	            </div>
+
+	            <!-- List group -->
+	            <ul class="list-group">
+	              {{ range .Top5Breweries }}
+	              <li class="list-group-item">{{ range $k, $v := . }}{{ $k }}<span class="badge badge-info">{{ $v }}</span>{{ end }}</li>
+	              {{ end }}
+	            </ul>
+	          </div>
+	        </div>
+	      </div>
+	    </div> <!-- .container -->
+
+	{{template "bootstrap_base_foot" .}}
+
+	{{define "bootstrap_base_head"}}
+	<!DOCTYPE html>
+	<html lang="en">
+	  <head>
+	    <meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <meta name="description" content="">
+	    <meta name="author" content="">
+	    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+
+	    <title>🍻 Chuck's Stats</title>
+
+	    <!-- Bootstrap core CSS -->
+	    <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+
+	    <!-- Custom styles for this template -->
+	    <link href="/public/css/slate.css" rel="stylesheet">
+	    <link href="/public/css/sticky-footer.css" rel="stylesheet">
+	    <link href="/public/css/fancy-badge.css" rel="stylesheet">
+
+	    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	    <!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+	  </head>
+	{{end}}
+
+	{{define "navbar"}}
+	      <div class="navbar navbar-default">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <a class="navbar-brand" href="/">Chuck's Stats</a>
+	        </div>
+	        <div class="navbar-collapse collapse navbar-responsive-collapse">
+	          <ul class="nav navbar-nav navbar-right">
+	            <li{{if eq .Page.Tab "today"}} class="active"{{end}}><a href="/today">Today</a></li>
+	            <li{{if eq .Page.Tab "thisweek"}} class="active"{{end}}><a href="/thisweek">This Week</a></li>
+	<!--             <li class="dropdown">
+	              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+	              <ul class="dropdown-menu">
+	                <li><a href="#">Chuck's 85th</a></li>
+	                <li><a href="#">Chuck's CD</a></li>
+	                <li class="divider"></li>
+	                <li><a href="#">Separated link</a></li>
+	              </ul>
+	            </li> -->
+	          </ul>
+	        </div><!-- /.nav-collapse -->
+	      </div>
+	{{ end }}
+
+	{{define "bootstrap_base_foot"}}
+
+	    <div id="footer">
+	      <div class="container">
+	        <p class="text-muted">Built for a love of beer, by <a href="https://twitter.com/dacort">@dacort</a>.</p>
+	      </div>
+	    </div>
+
+	    <!-- Bootstrap core JavaScript
+	    ================================================== -->
+	    <!-- Placed at the end of the document so the pages load faster -->
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+	  </body>
+	</html>
+	{{end}}
+`))
 
 type IndexPageVariables struct {
 	NewBeersToday []chucks.Beer
@@ -90,12 +427,100 @@ var fns = template.FuncMap{
 	},
 }
 
-var todayTemplate = template.Must(template.New("today.html").Funcs(fns).ParseFiles(
-	"../templates/today.html",
-	"../templates/bootstrap_base_head.html",
-	"../templates/navbar.html",
-	"../templates/bootstrap_base_foot.html",
-))
+var todayTemplate = template.Must(template.New("today.html").Funcs(fns).Parse(`
+	{{template "bootstrap_base_head" .}}
+	<body>
+	  <div class="container">
+	    {{ template "navbar" .}}
+	    <div class="row">
+	      <div class="col-md-6 col-md-offset-3">
+
+	        <ul class="list-group">
+	          <li class="list-group-item" style="background-color:lightslategrey;color:black"><strong>Beers on tap today - <span class="text-success">green</span> ones indicate newly on tap.</strong></li>
+	          {{ range $index, $beers := .TapList}}
+	          <li class="list-group-item">Tap {{$index}}) {{ range $beer_index, $beer := $beers }}{{ if last $beer_index $beers }}{{ if gt $beer_index 0}}<span class="text-success">{{end}}{{end}}{{ $beer.FullName }}{{ if last $beer_index $beers }}</span>{{end}}{{ if last $beer_index $beers }}. {{ else }}, {{ end }}{{ end }}</li>
+	          {{ end }}
+	        </ul>
+	      </div>
+	    </div>
+	  </div>
+
+	{{template "bootstrap_base_foot" .}}
+
+	{{define "bootstrap_base_head"}}
+	<!DOCTYPE html>
+	<html lang="en">
+	  <head>
+	    <meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <meta name="description" content="">
+	    <meta name="author" content="">
+	    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+
+	    <title>🍻 Chuck's Stats</title>
+
+	    <!-- Bootstrap core CSS -->
+	    <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+
+	    <!-- Custom styles for this template -->
+	    <link href="/public/css/slate.css" rel="stylesheet">
+	    <link href="/public/css/sticky-footer.css" rel="stylesheet">
+	    <link href="/public/css/fancy-badge.css" rel="stylesheet">
+
+	    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	    <!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+	  </head>
+	{{end}}
+
+	{{define "navbar"}}
+	      <div class="navbar navbar-default">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <a class="navbar-brand" href="/">Chuck's Stats</a>
+	        </div>
+	        <div class="navbar-collapse collapse navbar-responsive-collapse">
+	          <ul class="nav navbar-nav navbar-right">
+	            <li{{if eq .Page.Tab "today"}} class="active"{{end}}><a href="/today">Today</a></li>
+	            <li{{if eq .Page.Tab "thisweek"}} class="active"{{end}}><a href="/thisweek">This Week</a></li>
+	<!--             <li class="dropdown">
+	              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+	              <ul class="dropdown-menu">
+	                <li><a href="#">Chuck's 85th</a></li>
+	                <li><a href="#">Chuck's CD</a></li>
+	                <li class="divider"></li>
+	                <li><a href="#">Separated link</a></li>
+	              </ul>
+	            </li> -->
+	          </ul>
+	        </div><!-- /.nav-collapse -->
+	      </div>
+	{{ end }}
+
+	{{define "bootstrap_base_foot"}}
+
+	    <div id="footer">
+	      <div class="container">
+	        <p class="text-muted">Built for a love of beer, by <a href="https://twitter.com/dacort">@dacort</a>.</p>
+	      </div>
+	    </div>
+
+	    <!-- Bootstrap core JavaScript
+	    ================================================== -->
+	    <!-- Placed at the end of the document so the pages load faster -->
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+	  </body>
+	</html>
+	{{end}}
+`))
 
 type TodayPageVariables struct {
 	TapList map[int][]chucks.Beer
